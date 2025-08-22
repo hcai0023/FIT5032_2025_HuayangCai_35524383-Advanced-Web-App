c02666671cfbb2987e4809d14a76d794733e306e
Huayng Cai <hcai0023@student.monash.edu>
2025-08-22 18:04:28 +0800

    set js

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: './',
   build:{
     outDir: 'dist',
     assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        // 确保资源路径正确
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
   },
   plugins: [
     vue(),
